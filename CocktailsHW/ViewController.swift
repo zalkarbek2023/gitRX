16e004d430ca3e91c8f8e236d4af54cddb7bc82f
ZalkarbekArapbaev <zalkarbek2022@gmail.com>
2023-02-22 13:26:55 +0600

    Initial Commit

---
+++ CocktailsHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CocktailsHW
+//
+//  Created by zalkarbek on 22/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
